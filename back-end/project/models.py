f00ef70ae5fc4dbdf8b4a744351369b1c8ff11e4
Emmanuel Nunez <mannynunezr@gmail.com>
2022-05-02 19:56:24 -0400

    Iffat created projects and profile models

--- back-end/project/models.py
+++ back-end/project/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.forms import DateTimeField
+from pytz import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+
+    bio = models.TextField(max_length=500)
+    github = models.CharField(max_length=128)
+    image = models.ImageField(blank=True, upload_to='blog_images')
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return self.bio
+
+
+class Projects(models.Model):
+    user = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, related_name='user')
+    title = models.CharField(max_length=128)
+    github_link = models.CharField(max_length=128)
+    publish_date = DateTimeField(default=timezone.now)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
