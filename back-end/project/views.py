0265a90bd8b22e6710b60d2fcefd5378e2886f34
ehlee95 <eric95lee@gmail.com>
2022-05-03 10:19:50 -0400

    removed redundant redirect file, updated project views and serializers

--- back-end/project/views.py
+++ back-end/project/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
 
-# Create your views here.
+from .serializers import ProjectSerializer
+from .models import Project
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
--- back-end/user_profile/views.py
+++ back-end/user_profile/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from .serializers import UserSerializer
 from django.contrib.auth.models import User
 
-# Create your views here.
-
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
