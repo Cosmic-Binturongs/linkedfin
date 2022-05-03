3be3dd5a4366f3b837f4d221ced2cf57dfa0dddb
Mike Delucia <mikey970@yahoo.com>
2022-05-03 10:24:35 -0400

    rearranged views and serializers

--- back-end/user_profile/views.py
+++ back-end/user_profile/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from .serializers import UserSerializer
+from .serializers import ProfileSerializer, UserSerializer
 from django.contrib.auth.models import User
+from .models import User_profile
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = User_profile.objects.all()
+    seerializer_class = ProfileSerializer
