fb1006496c6ba3f880f389e2213a41272a4c7bc6
Iffat Hossain <iffathossain@iffats-air.mynetworksettings.com>
2022-05-04 20:53:32 -0400

    updates link to wrap around the profile.name

--- front-end/src/components/ProfileComp.jsx
+++ front-end/src/components/ProfileComp.jsx
@@ -6,18 +6,18 @@ export default function ProfileComp({profile}) {
 
 
   return (
-     
-   <Link to={`/profile/${profile._id}`}>
-      <div className='profile-component'>
-        <h3 className='profile-component-name'>{profile.name}</h3>
-        <img className='profile-component-image' src={profile.image} />
-        <p className='profile-component-bio'>{profile.bio}</p>
-        <a className='profile-component-github' href={`${profile.github}`}>GitHub</a>
+    <div className='profile-component'>
+     <Link to={`/profile/${profile._id}`}>  
+      <h3 className='profile-component-name'>{profile.name}</h3>
+     </Link>
+     <img className='profile-component-image' src={profile.image} />
+     <p className='profile-component-bio'>{profile.bio}</p>
+     <a className='profile-component-github' href={`${profile.github}`}>GitHub</a>
       
-        {/* will try once user is added */}
-        {/* <a className='profile-component-mailTo' href={`mailto:${profile.user.mail}`}>E-Mail</a> */}
+     {/* will try once user is added */}
+     {/* <a className='profile-component-mailTo' href={`mailto:${profile.user.mail}`}>E-Mail</a> */}
         
-      </div>
-   </Link>
+    </div>
+ 
   )
 }
