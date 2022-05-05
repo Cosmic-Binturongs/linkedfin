799675570ddb81a68e622710f0d0fa44186aa369
ehlee95 <eric95lee@gmail.com>
2022-05-05 16:05:43 -0400

    add footer

---
+++ front-end/src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import './footer.css'
+import { useNavigate } from 'react-router-dom'
+
+
+export default function footer() { 
+
+
+  return (
+    <div className="footer">
+      <div>Github</div>
+      <div>Team</div>
+    </div>
+  )
+}
