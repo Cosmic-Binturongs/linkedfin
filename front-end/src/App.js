b5441d17a34240ee57f88b84d954f068f32a9445
Mike Delucia <mikey970@yahoo.com>
2022-05-04 21:41:55 -0400

    created sign in componet and css file

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
+import './components/signin.js'
+import Signin from './components/signin.js';
 
 function App() {
   return (
-    <div><h1>test</h1></div>
+    <div></div>
   );
 }
 
---
+++ front-end/src/components/signin.js
@@ -0,0 +1,18 @@
+//fire
+import './signin.css';
+function Signin(props) {
+  return (
+    <div className='signinContainer'>
+      <h1 className='topText'>Sign In</h1>
+      <div className="signinInput">
+        <input id='textfield' className="usernameField" type='text' placeholder="Username"></input>
+        <input id='textfield'className="passwordField" type='text' placeholder="Password"></input>
+        <div>
+        <button className='signinButton'>Sign in</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Signin
