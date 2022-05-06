b879dae8d98f4bfc0fb2ee6e8ba0e3cf0b9774ad
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-05-06 16:06:37 -0400

    changes in the routes

--- back-end/linkedfin_backend/urls.py
+++ back-end/linkedfin_backend/urls.py
@@ -20,9 +20,10 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api_auth/', include('rest_framework.urls')),
-    path('accounts/', include('projects.urls')),
+    path('', include('projects.urls')),
     path('profile/', include('user_profile.urls'))
 ]
 
 # Add regular expression for catch-all
+
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
