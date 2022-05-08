b8aaf8ed6b53b26d01c8838dc9ba29fabadd93b6
KW <88893976+KdoubleUs@users.noreply.github.com>
2022-05-08 11:49:20 -0400

    added NOTFOUND.JSX

---
+++ front-end/src/components/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./NotFound.css";
+function NotFound() {
+  return <div className="error-page">403 Not Authenticated Please Log in</div>;
+}
+
+export default NotFound;
